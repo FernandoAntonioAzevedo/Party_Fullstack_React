22b1e9ed830c507d1cbd90da9b42ebe34c0c441b
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-08-16 17:44:30 -0300

    finalizando conexão ao banco de dados e dando inicio a criação dos models

---
+++ backend/db/conn.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+async function main() {
+    
+    try {
+        mongoose.set("strictQuery", true);
+        
+        await mongoose.connect(
+            "mongodb+srv://beratechsolutions:L0leKayJICU7zmAc@cluster0.ktqer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        );
+
+        console.log("Conectado ao banco!");
+
+    } catch (error) {
+        console.log(`Erro: ${error}`);
+    }
+}
+
+
+module.exports = main; 
